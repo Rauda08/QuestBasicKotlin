b8d7e821e281205f2e965c15ba224437cbc7c5e1
Rauda08 <raudajnnh08@gmail.com>
2024-10-11 23:24:59 +0700

    pemeriksaan null dan penggunaan safe call operator

--- app/src/main/java/com/example/pam1/NullSafety.kt
+++ app/src/main/java/com/example/pam1/NullSafety.kt
@@ -10,3 +10,28 @@ fun nullSafety() {
     var nullable: String? = "You can keep a null here"
 // This is OK
     nullable = null
+
+    // Check for null in conditions
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+    } else {
+        println("inferredNonNull is not null")
+    }
+
+// Safe call operator
+    println(neverNull.length) // 18
+    println(nullable?.length) // null
+
+
+    // notNull doesn't accept null values
+    fun strLength(notNull: String): Int {
+        return notNull.length
+    }
+
+    println(strLength(neverNull)) //18
+
+}
+
+fun main() {
+    nullSafety()
+}
