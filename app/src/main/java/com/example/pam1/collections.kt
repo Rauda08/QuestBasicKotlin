cad1dc295dd0a35121de5efe863a6f41ef87793a
Rauda08 <raudajnnh08@gmail.com>
2024-10-11 22:55:51 +0700

    menambah implementasi List, Set, dan Map dengan operasi mutable dan read-only

--- app/src/main/java/com/example/pam1/collections.kt
+++ app/src/main/java/com/example/pam1/collections.kt
@@ -70,4 +70,37 @@ fun ContohSet() {
 // Map Read-Only menggunakan mapOf
 // Map Mutable menggunakan mutableMapOf
 
+fun ContohMap() {
 
+    println()
+    println("=== Map ===")
+    // Map Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square " to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    // Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    // Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Mengubah data di dalam Map Mutable
+    shape["Square"] = 5
+    println(shape)
+
+    // Map Read-Only
+    val shapesLocked: Map<String, Int> = shape
+    println(shapesLocked)
+}
+
+fun main() {
+    ContohList()
+    ContohSet()
+    ContohMap()
+}
