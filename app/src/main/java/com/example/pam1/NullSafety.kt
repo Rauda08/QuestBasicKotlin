2f579ed1294132299ed8eda6ada31ca7f813c073
Rauda08 <raudajnnh08@gmail.com>
2024-10-11 23:23:42 +0700

    membuat null safety dengan variabel non-nullable dan nullable

---
+++ app/src/main/java/com/example/pam1/NullSafety.kt
@@ -0,0 +1,12 @@
+package com.example.pam1
+fun nullSafety() {
+// neverNull has String type
+    var neverNull: String = "This can't be null"
+
+// Throws a compiler error
+// neverNull = null
+
+// nullable has nullable String type
+    var nullable: String? = "You can keep a null here"
+// This is OK
+    nullable = null
